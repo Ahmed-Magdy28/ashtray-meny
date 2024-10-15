2305ac2726ae18c0fe661ae1c452eabc47cebcc8
ahmedtiko <ahmedmm_tarek@hotmail.com>
2024-10-15 23:01:22 +0300

    including the core app's URLs

--- back-end/ashtray_meny/app/urls.py
+++ back-end/ashtray_meny/app/urls.py
@@ -27,6 +27,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),  # Include core app URLs
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login route
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token route
     path('api/user/', include('user.urls')),  # User-related routes
